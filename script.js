59379c0d24f4d9ad431377141c955974c468d41e
Ivyan <116150104+26733@users.noreply.github.com>
2024-07-18 16:23:28 +0530

    Done

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.getElementById("greetingBox").addEventListener("mouseenter", function() {
+document.getElementById("greetingText").innerHTML = "👋 Hello!";
+});
+            
+document.getElementById("greetingBox").addEventListener("mouseleave", function() {
+document.getElementById("greetingText").innerHTML = "🙏 नमस्कार!";
+});
+
+
+
+
